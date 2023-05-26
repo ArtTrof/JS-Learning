fa0bb777b46d8c4a1e51b98799b7cea21065a1b3
ArtTrof <randallsng@gmail.com>
2023-05-26 18:22:24 +0200

    variables js

---
+++ JavaScriptFundamentals/Variables.js
@@ -0,0 +1,12 @@
+let message;
+message = 'Hello World!';
+alert(message);
+let user = 'Oleg', age = 25;
+const helloWorld = 'this is constant Hello world!';
+// practice
+let admin, name;
+name = 'John';
+admin=name;
+alert(admin);
+
+const homePlanet='Earth';
