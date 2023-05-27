4ef6d2b46be968b458665fe22b85f6c561c7d4a5
ArtTrof <randallsng@gmail.com>
2023-05-27 13:39:34 +0200

    Interaction done

---
+++ JavaScriptFundamentals/Interaction.js
@@ -0,0 +1,10 @@
+//alert
+alert("Hello World");
+//prompt
+let age = prompt('How old are you?',100);
+alert('You are ' + age + ' years old');
+let test = prompt("Test");
+let test2 = prompt("Test", '');
+//confirm 
+let isBoss = confirm('Are you a boss?');
+alert(isBoss);
